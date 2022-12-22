21baceae6d0a5d29bdcc0bca5bc63a38c2e2f2bd
matte-ek <49276951+matte-ek@users.noreply.github.com>
2022-12-22 18:00:42 +0100

    Fix vote skip not being reset when host is self skipping

--- BanchoMultiplayerBot/Behaviour/AutoHostRotateBehaviour.cs
+++ BanchoMultiplayerBot/Behaviour/AutoHostRotateBehaviour.cs
@@ -123,6 +123,8 @@ public class AutoHostRotateBehaviour : IBotBehaviour
 
                     OnQueueUpdated();
 
+                    _playerSkipVote.Reset();
+
                     return;
                 }
             }
