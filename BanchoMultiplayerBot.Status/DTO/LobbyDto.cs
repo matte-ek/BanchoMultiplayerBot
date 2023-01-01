2bfa77df0712a4f76a8977ae25c222dce5af5638
matte-ek <49276951+matte-ek@users.noreply.github.com>
2023-01-01 21:16:23 +0100

    Add initial status project

---
+++ BanchoMultiplayerBot.Status/DTO/LobbyDto.cs
@@ -0,0 +1,15 @@
+﻿namespace BanchoMultiplayerBot.Status.DTO;
+
+public class LobbyDto
+{
+
+    public string Name { get; set; } = string.Empty;
+    
+    public int? BeatmapId { get; set; }
+    public int? BeatmapSetId { get; set; }
+    
+    public int Players { get; set; }
+    
+    public int GamesPlayed { get; set; }
+    
+}
---
+++ BanchoMultiplayerBot.Status/DTO/StatusDto.cs
@@ -0,0 +1,20 @@
+﻿namespace BanchoMultiplayerBot.Status.DTO;
+
+public class StatusDto
+{
+    
+    public bool Status { get; set; }
+    
+    public DateTime StartTime { get; set; }
+    
+    public DateTime LastNetworkIssueTime { get; set; }
+
+    public int Warnings { get; set; }
+    
+    public float AvgPlayers { get; set; }
+    
+    public int GamesPlayed { get; set; }
+    
+    public LobbyDto[]? Lobbies { get; set; }
+    
+}
