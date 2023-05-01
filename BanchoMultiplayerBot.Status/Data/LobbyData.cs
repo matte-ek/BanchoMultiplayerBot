22aea592e37fac24bd8558939db32edd766ff9e7
matte-ek <matero3210@gmail.com>
2023-05-01 22:41:01 +0200

    Add status page

---
+++ BanchoMultiplayerBot.Status/Data/LobbyData.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazor;
+
+namespace BanchoMultiplayerBot.Status.Data
+{
+    public class LobbyData
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Players { get; set; }
+
+        public double AveragePlayers {get; set; }
+
+        public int GamesLastHour { get; set; }
+
+        public string MapName { get; set; }
+        public int MapId { get; set; }
+        public int MapSetId { get; set; }
+
+        public List<ChartSeries> Chart { get; set; } = null!;
+    }
+}
---
+++ BanchoMultiplayerBot.Status/Data/StatisticsManagerService.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+using BanchoMultiplayerBot.Host.Web.Statistics;
+using Newtonsoft.Json;
+
+namespace BanchoMultiplayerBot.Status.Data
+{
+    public class StatisticsManagerService
+    {
+        private readonly HttpClient _httpClient = new();
+
+        public List<StatisticsMinuteSnapshot?> StatisticsMinuteSnapshots { get; private set; } = new();
+
+        public void Start()
+        {
+            Task.Run(StatisticsRetrieverTask);
+        }
+
+        public async Task StatisticsRetrieverTask()
+        {
+            while (true)
+            {
+                var data = await _httpClient.GetAsync("http://localhost:5000/api/statistics");
+
+                StatisticsMinuteSnapshot? minuteSnapshot = null;
+
+                if (data.IsSuccessStatusCode)
+                {
+                    try
+                    {
+                        minuteSnapshot = JsonConvert.DeserializeObject<StatisticsMinuteSnapshot>(await data.Content.ReadAsStringAsync());
+                    }
+                    catch (Exception)
+                    {
+                        // ignored
+                    }
+                }
+
+                StatisticsMinuteSnapshots.Insert(0, minuteSnapshot);
+
+                if (StatisticsMinuteSnapshots.Count > (60 * 24) + 1)
+                {
+                    StatisticsMinuteSnapshots.RemoveAt(StatisticsMinuteSnapshots.Count - 1);
+                }
+
+                await Task.Delay(TimeSpan.FromMinutes(1));
+            }
+        }
+    }
+}
