a82300224c09faa9594aaf0152636627a4d3b158
matte-ek <matero3210@gmail.com>
2023-09-23 19:34:57 +0200

    Add !map command for DMs

--- BanchoMultiplayerBot/GlobalCommands.cs
+++ BanchoMultiplayerBot/GlobalCommands.cs
@@ -1,4 +1,6 @@
+using System.Globalization;
 using System.Text;
+using BanchoMultiplayerBot.Extensions;
 using BanchoSharp.Interfaces;
 using BanchoSharp.Multiplayer;
 using Serilog;
@@ -89,6 +91,46 @@ public class GlobalCommands
                 }
                 
                 _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient, response.ToString());
+            }
+            
+            // This is more or less out of scope for this bot, but I've added it anyway for DMs
+            // however it's not documented in COMMANDS.md.
+            if ((msg.Content.ToLower().Equals("!map") || msg.Content.ToLower().Equals("!m")) && msg.IsDirect)
+            {
+                if (_bot.PerformancePointCalculator == null)
+                {
+                    return;
+                }
+  
+                var recentScore = await _bot.OsuApi.GetRecentScore(msg.Sender);
+                if (recentScore == null)
+                {
+                    _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient,"Unable to get recent score details.");
+                    return;
+                }
+
+                var beatmapInformation =
+                    await _bot.OsuApi.GetBeatmapInformation(int.Parse(recentScore.BeatmapId!));
+                
+                if (beatmapInformation == null)
+                {
+                    _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient,"Unable to get beatmap information.");
+                    return;
+                }
+
+                var ppInformation = await _bot.PerformancePointCalculator!.CalculatePerformancePoints(int.Parse(recentScore.BeatmapId!));
+                if (ppInformation == null)
+                {
+                    _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient,"Unable to get calculate pp information.");
+                    return;
+                }
+
+                var starRating = Math.Round(float.Parse(beatmapInformation.DifficultyRating!, CultureInfo.InvariantCulture), 2);
+                var length = TimeSpan.FromSeconds(int.Parse(beatmapInformation.TotalLength ?? "0", CultureInfo.InvariantCulture));
+                
+                _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient, $"[https://osu.ppy.sh/b/{recentScore.BeatmapId} {beatmapInformation.Artist} - {beatmapInformation.Title} [{beatmapInformation.Version ?? string.Empty}]]");
+                _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient,$"(Star Rating: {starRating:.0#} | {beatmapInformation.GetStatusString()} | Length: {length:mm\\:ss} | BPM: {beatmapInformation.Bpm})");
+                _bot.SendMessage(msg.IsDirect ? msg.Sender : msg.Recipient, $"(AR: {beatmapInformation.DiffApproach} | CS: {beatmapInformation.DiffSize} | OD: {beatmapInformation.DiffOverall} | HP: {beatmapInformation.DiffDrain} | 100%: {ppInformation.Performance100}pp | 98%: {ppInformation.Performance98}pp | 95%: {ppInformation.Performance95}pp)");
             }
         }
         catch (Exception e)
