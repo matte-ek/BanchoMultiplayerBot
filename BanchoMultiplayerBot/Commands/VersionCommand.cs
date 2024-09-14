bd6dd96d28c951a00a2b499972984957b2280bbc
matte-ek <49276951+matte-ek@users.noreply.github.com>
2024-09-14 16:38:03 +0200

    Add version command

---
+++ BanchoMultiplayerBot/Commands/VersionCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using BanchoMultiplayerBot.Data;
+using BanchoMultiplayerBot.Interfaces;
+
+namespace BanchoMultiplayerBot.Commands;
+
+public class VersionCommand : IPlayerCommand
+{
+    public string Command => "version";
+
+    public List<string>? Aliases => [];
+
+    public bool AllowGlobal => true;
+
+    public bool Administrator => false;
+
+    public int MinimumArguments => 0;
+
+    public string? Usage => null;
+
+    public Task ExecuteAsync(CommandEventContext message)
+    {
+        var version = Assembly.GetExecutingAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+
+        if (version == null)
+        {
+            return Task.CompletedTask;
+        }
+
+        var commitHashBegin = version.IndexOf('+') + 1;
+        var commitHash = version[commitHashBegin..];
+        var commitHashSubset = version[commitHashBegin..(commitHashBegin + 7)];
+        
+        message.Reply($"BanchoMultiplayerBot@[https://github.com/matte-ek/BanchoMultiplayerBot/commit/{commitHash} {commitHashSubset}], current uptime: {Process.GetCurrentProcess().StartTime:HH:mm:ss}");
+            
+        return Task.CompletedTask;
+    }
+}
